763a1ec56de27bb1295ce78d741c2f0b81ff034f
are yaar <tanmaybhanushali2003@gmail.com>
2026-06-12 20:52:38 +0530

    add import to controller

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -1,3 +1,4 @@
+require("pdf-parse/worker");
 const pdfParse = require("pdf-parse");
 const { generateInterviewReport, generateResumePdf } = require("../services/ai.service")
 const interviewReportModel = require("../models/interviewReport.model.js");
